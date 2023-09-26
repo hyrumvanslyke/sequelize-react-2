06f7c816a9de2a0038a9cdfa53119dd9a9235e1c
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-09-26 16:21:39 -0400

    Associations worked on

--- app.js
+++ app.js
@@ -1,34 +1,52 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-const seq = require('./util/database');
+const errorController = require("./controllers/error");
+const seq = require("./util/database");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
-User.hasMany(Product)
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
 
-seq.sync({force: true}).then(result=>{
-    app.listen(3000)
-}).catch(err=>{
-    console.log(err)
-})
-
-
+seq
+  .sync({ force: true })
+  .then((result) => {
+    return User.findById(1)
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "max", email: "bigboisalad@yahoo.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
